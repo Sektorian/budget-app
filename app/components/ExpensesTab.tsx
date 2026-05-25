00d9ead6c6e843afb69fc8a7108e98d280c5d03a
Sektorian <g.sektorian@gmail.com>
2026-05-25 15:15:52 +0300

    add plan

---
+++ app/components/ExpensesTab.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ExpensesTab() {
+  return (
+    <div className="empty-tab">
+      Расходы
+    </div>
+  );
+}
---
+++ app/components/StatsTab.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function StatsTab() {
+  return (
+    <div className="empty-tab">
+      Статистика
+    </div>
+  );
+}
